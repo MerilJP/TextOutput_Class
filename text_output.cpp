1f34f3ded9036eefdcb1b1e2f26ddb6e0df1f15d
Meril <yamato0130.0w0@gmail.com>
2017-06-03 13:12:51 +0900

    Add files via upload

---
+++ text_output.cpp
@@ -0,0 +1,82 @@
+#include "text_output.hpp"
+
+TextOutput::TextOutput()
+{
+}
+
+TextOutput::~TextOutput()
+{
+}
+
+//引数:(タイトル,本文)
+void TextOutput::WriteText(std::string Title, std::string TextMain)
+{
+	std::ofstream ofs;
+
+	//拡張子が指定されていなければ追加
+	this->addExtension(Title);
+	//出力ファイル名
+	ofs.open(Title, std::ios::out);
+	//ファイルを出力
+	ofs << TextMain << std::endl;
+	//後処理
+	ofs.close();
+}
+
+//引数:(タイトル,本文,パス)
+void TextOutput::WriteText(std::string Title, std::string TextMain, int Path)
+{
+	std::ofstream ofs;
+	
+	//拡張子が指定されていなければ追加
+	this->addExtension(Title);
+
+	//出力パス
+	switch (Path)
+	{
+	case DESKTOP:
+		ofs.open(this->getDeskPath() + Title, std::ios::out);
+		break;
+	default:
+		ofs.open(this->getDeskPath() + Title, std::ios::out);
+		break;
+	}	
+	//ファイルを出力
+	ofs << TextMain << std::endl;
+	//後処理
+	ofs.close();
+}
+
+//デスクトップパスを取得
+std::string TextOutput::getDeskPath()
+{
+	std::wstring wsBuf;
+	TCHAR szPath[MAX_PATH];
+
+	SHGetSpecialFolderPath(NULL, szPath, CSIDL_DESKTOP, false);
+	wsBuf = szPath;
+	
+	std::string stBuf(wsBuf.begin(),wsBuf.end());
+	for (int i = 0; i <= (int)stBuf.length(); i++) 
+	{
+		if (stBuf[i] == '\\')
+		{
+			stBuf.insert(stBuf.begin() + i, '\\');
+			i++;
+		}
+	}
+	stBuf = stBuf + "\\\\";
+
+	return stBuf;
+}
+
+//ファイル拡張子の追加
+void TextOutput::addExtension(std::string Title)
+{
+	//拡張子が設定されていなければ末尾に追加する
+	std::string::size_type ret = Title.find(".txt");
+	if (ret == std::string::npos)
+	{
+		Title = Title + ".txt";
+	}
+}
---
+++ text_output.hpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <shlobj.h>
+#include <string>
+
+
+enum 
+{
+  DESKTOP
+};
+
+
+#ifndef __TEXTOUTPRINT_H_
+#define __TEXTOUTPRINT_H_
+
+
+
+class TextOutput{
+public:
+
+	//コンストラクタ
+	TextOutput();
+	//デストラクタ
+	~TextOutput();
+
+	//引数:(タイトル,本文)
+	void WriteText(std::string,std::string);
+	//引数:(タイトル,本文,パス)
+	void WriteText(std::string, std::string , int);
+
+private:
+
+	//デスクトップのパスを取得
+	std::string getDeskPath();
+	//拡張子が指定されていなければ追加
+	void addExtension(std::string);
+
+};
+#endif
